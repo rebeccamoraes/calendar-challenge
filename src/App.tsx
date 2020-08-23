85e7473e6e62a432832b43dd9f16d06a31d4a73c
Rebecca Moraes <rebecca@ufg.br>
2020-08-23 16:06:36 -0300

    Create global style with styled-components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes />
+
+    <GlobalStyle />
   </BrowserRouter>
 );
 
